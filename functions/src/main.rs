129243b6bd433fdedc58b6dc6286ffe2e7e6d05f
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 10:00:25 +0300

    fn args

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("Значение x равно {}", x);
+    println!("Значение y равно {}", y);
 }
