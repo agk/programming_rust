a6d93e3497c360869ca25d8e3ab77a3868926452
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 08:40:21 +0300

    кортежи3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     // составные типы
     // кортежный тип
-    // let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("Значение y равно {}", y);
-
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
 }
