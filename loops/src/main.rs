d3a1d21cb3b7a84be632c926e68ba07a06f6cb6b
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 10:42:04 +0300

    for rev

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    for element in a.iter() {
-        println!("значение равно {}!", element);
+    for number in (1..4).rev() {
+        println!("{}!", number);
 
     }
+    println!("Поехали!!!")
     
 }
