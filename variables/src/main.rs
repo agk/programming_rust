d3ccfc9412af84d49ec956e80d33c2e701757636
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 08:51:02 +0300

    arr

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
-    let fist = a[0];
-    let second = a[1];
+    let index = 10;
+    
+    let element = a[index];
 
+    println!("Значение элемента равно {}", element);
+
+    
 }
