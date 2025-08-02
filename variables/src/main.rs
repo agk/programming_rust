727038088d12171361966f23505a32fc2fa62e3d
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 08:42:33 +0300

    array

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // составные типы
     // кортежный тип
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let x: (i32, f64, u8) = (500, 6.4, 1);
     let five_hundred = x.0;
     let six_point_four = x.1;
     let one = x.2;
