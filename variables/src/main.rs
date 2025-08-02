368db2cf913c93b2aa828ececb66813149aea436
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 08:43:52 +0300

    --

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    // составные типы
-    // кортежный тип
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-    let five_hundred = x.0;
-    let six_point_four = x.1;
-    let one = x.2;
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let month = ["январь","февраль"]
+
 }
