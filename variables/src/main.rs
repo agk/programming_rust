38435d2297e6cd7647a55969b8f8b16edc9bc2bc
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 08:26:51 +0300

    + числовые операции

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,5 +14,19 @@ fn main() {
     let x = 2.0; // f64
     let y: f32 = 3.0; // f32
 
-    
+    // сложение
+    let sum = 5 + 10;
+
+    // вычитание
+    let difference = 95.5 - 4.3;
+
+    // умножение
+    let product = 4 * 30;
+
+    // деление
+    let quotient = 56.7 / 32.2;
+
+    // остаток
+    let remainder = 43 % 5;
+     
 }
