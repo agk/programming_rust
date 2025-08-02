324d81decd094e784a5c9794a3e8ad8095b03f7f
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 10:33:16 +0300

    while

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let mut counter = 0;
+    let mut number = 3;
 
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-    println!("Результат равен {}", result);
-    
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+
+    }
+    println!("Поехали!!!");
+
 }
