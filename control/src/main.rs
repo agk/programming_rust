07e51ce0bbd3269317f3df52e1e8872d38ed8a40
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-02 10:19:40 +0300

    if let

--- control/src/main.rs
+++ control/src/main.rs
@@ -1,16 +1,10 @@
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+    println!("значение  числа равно {}", number);
 
-    if number % 4 == 0 {
-        println!("число делится на 4");
-    }
-    else if number % 3 == 0 {
-        println!("число делится на 3");
-    }
-    else if number % 2 == 0 {
-        println!("число делится на 2");
-    }
-    else {
-        println!("число не делится на 4, 3 и 2");
-    }
 }
