24bc0db0a1f22e885dfea2d3f57c5d5ad355eb41
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-09 14:18:15 +0300

    list 5.3.

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -6,11 +6,13 @@ fn main() {
         active: bool,
     }
 
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("primer@example.com"),
         username: String::from("primerusername123"),
         active: true,
         sign_in_count: 1,
     }
+
+    user1.email = String::from("antheremail@example.com");
     
 }
