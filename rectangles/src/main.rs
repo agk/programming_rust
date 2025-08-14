966c9609dd25a95fcd824d77c38f21d8eeec62e1
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2025-08-14 14:26:44 +0300

    listing 5.10

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,14 +1,19 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle { width: 30, height: 50 };
 
     println!(
         "Площадь прямоугольника равна {} квадратным пискселам.",
-        area(rect1)
+        area(&rect1)
     );
 
 
 }
 
-fn area(demensions: (u32, u32)) -> u32 {
-    demensions.0 * demensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
