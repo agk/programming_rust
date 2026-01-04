f8a04d296cecfd2f02e5afb71d2df6c91434db16
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-04 16:35:43 +0300

    + add ownership examples

---
+++ ownership/target/debug/deps/ownership-be4e861eae33868f.d
@@ -0,0 +1,5 @@
+/Users/agk/code_src/src/github.com/programming_rust/ownership/target/debug/deps/ownership-be4e861eae33868f.d: src/main.rs
+
+/Users/agk/code_src/src/github.com/programming_rust/ownership/target/debug/deps/ownership-be4e861eae33868f: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/agk/code_src/src/github.com/programming_rust/ownership/target/debug/ownership: /Users/agk/code_src/src/github.com/programming_rust/ownership/src/main.rs
