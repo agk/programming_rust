bcad3d16466da5a51b6100842ad24d48403370e2
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-05 18:02:30 +0300

    + references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    
+    println!("Длина '{}' равна {}.", s1, len);
+
+    // let s = String::from("hello");
+    // change(&s);
+
 }
+
+fn calculate_length(s: &String) -> usize { // s — это ссылка на экземпляр
+    // типа String
+    s.len()
+}   // Здесь s выходит из области видимости. Но поскольку она не владеет тем,
+    // на что она ссылается, ничего не происходит.
+
+
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+//     // Ссылки, точно так же, как и переменные, по умолчанию неизменяемы. 
+//     // Мы не можем модифицировать то, на что у нас есть ссылка
+// }
