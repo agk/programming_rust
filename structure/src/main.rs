a5ae267c3ab17e5e0e405a5d3a62f80f8bc727ed
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-05 19:09:14 +0300

    +

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -5,13 +5,33 @@ struct User {
     active: bool,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
 
-    let user1 = User {
-        email: String:from("primer@example.com"),
-        username: String.from("primerusername123"),
+    let mut user1 = User {
+        email: String::from("primer@example.com"),
+        username: String::from("primerusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let mut _user2 = build_user(String::from("anotheremail@example.com"), String::from("agk"));
+
+    // Использование кортежных структур без именованных полей для создания разных типов
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
         active: true,
         sign_in_count: 1,
     }
-
 }
